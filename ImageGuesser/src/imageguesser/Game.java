6b75916c23c91bb9350d9bf6f56ac2b1d5721f80
aRahman3341 <160831983+aRahman3341@users.noreply.github.com>
2025-04-28 17:52:52 +0700

    Nambah code jawaban dan code foto

--- ImageGuesser/src/imageguesser/Game.java
+++ ImageGuesser/src/imageguesser/Game.java
@@ -25,9 +25,16 @@ public class Game extends javax.swing.JFrame {
         life.setText(String.valueOf(lifePoint));
         
         daftarSoal = new Soal[]{
-            new Soal(1, "Ayam Bakar", new ImageIcon(getClass().getResource("/imageguesser/gambar/AyamKakiEmpat.jpeg"))),
-            new Soal(2, "Ayam Goreng", new ImageIcon("tes.png")),
-            new Soal(3, "Ayam Enak", new ImageIcon("tes.png")),
+            new Soal(1, "Sapi Kepala Ayam", new ImageIcon(getClass().getResource("/imageguesser/gambar/SapiKepalaAyam.jpeg"))),
+            new Soal(2, "Monyet", new ImageIcon("/imageguesser/gambar/Monyet.jpeg")),
+            new Soal(3, "Kuda Kepala Angsa", new ImageIcon("/imageguesser/gambar/Kuda Kepala Angsa.jpeg")),
+            new Soal(4, "Kucing Kepala Ayam", new ImageIcon("/imageguesser/gambar/Kucing Kepala Ayam.jpeg")),
+            new Soal(5, "Kucing Kaki Dua", new ImageIcon("/imageguesser/gambar/Kucing Kaki Dua.jpeg")),
+            new Soal(6, "Kelinci Kaki Dua", new ImageIcon("/imageguesser/gambar/Kelinci Kaki Dua.jpeg")),
+            new Soal(7, "Kucing Berenang", new ImageIcon("/imageguesser/gambar/Kucing Berenang.jpeg")),
+            new Soal(8, "Hiu Kaki Empat", new ImageIcon("/imageguesser/gambar/Hiu Kaki Empat.jpeg")),
+            new Soal(9, "Burung Kepala Kucing", new ImageIcon("/imageguesser/gambar/Burung Kepala Kucing.jpeg")),
+            new Soal(10, "Angsa Kaki Empat", new ImageIcon("/imageguesser/gambar/Angsa Kaki Empat.jpeg")),
         };
         
         tampilkanSoal();
@@ -88,6 +95,8 @@ public class Game extends javax.swing.JFrame {
             }
         });
 
+        hati.setIcon(new javax.swing.ImageIcon("C:\\Users\\Administrator\\Documents\\NetBeansProjects\\Image-Guesser-JAVA-master\\ImageGuesser\\Image-Guesser-JAVA\\ImageGuesser\\Image-Guesser-JAVA\\ImageGuesser\\src\\imageguesser\\gambar\\Nyawa.png")); // NOI18N
+
         life.setFont(new java.awt.Font("Verdana", 1, 36)); // NOI18N
         life.setForeground(new java.awt.Color(255, 255, 255));
         life.setText("3");
@@ -109,10 +118,20 @@ public class Game extends javax.swing.JFrame {
         jawaban.setRows(1);
         jScrollPane2.setViewportView(jawaban);
 
+        areaGambar.setIcon(new javax.swing.ImageIcon("C:\\Users\\Administrator\\Documents\\NetBeansProjects\\Image-Guesser-JAVA-master\\ImageGuesser\\Image-Guesser-JAVA\\ImageGuesser\\Image-Guesser-JAVA\\ImageGuesser\\src\\imageguesser\\gambar\\Sapi Kepala Ayam.jpeg")); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(246, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(174, 174, 174)
+                .addComponent(life, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(hati)
+                .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -122,49 +141,36 @@ public class Game extends javax.swing.JFrame {
                         .addGap(350, 350, 350)
                         .addComponent(skor))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(138, 138, 138)
-                        .addComponent(areaGambar, javax.swing.GroupLayout.PREFERRED_SIZE, 505, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(0, 157, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(220, 220, 220)
+                        .addComponent(areaGambar)))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(269, 269, 269)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(submitBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(291, 291, 291))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(life, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(194, 194, 194)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(hati, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(submitBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(life, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(gameUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(hati, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addComponent(gameUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(life, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(hati))
                 .addGap(18, 18, 18)
                 .addComponent(skor)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(areaGambar, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(92, 92, 92)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(27, 27, 27)
                 .addComponent(submitBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(193, Short.MAX_VALUE))
+                .addContainerGap(104, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
